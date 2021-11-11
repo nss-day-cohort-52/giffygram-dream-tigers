4918527c09731b50be2e2ed9c694b8db69fbdb0d
Sean Derevjanik <sean.derevjanik@gmail.com>
2021-11-11 13:18:56 -0700

    isolated footer in its own module and changed order to prevent element merging

---
+++ src/scripts/nav/Footer.js
@@ -0,0 +1,13 @@
+export const Footer = () => { 
+return  `<footer class="footer">
+            <div>
+                <label>Posted By:</label>
+                fill with function to create select element
+            </div>
+            <div>
+                <label>Show Favorites Only</label>
+                <input type=checkbox name="favorites"></input>
+            </div>
+        </footer>`
+    
+    }
