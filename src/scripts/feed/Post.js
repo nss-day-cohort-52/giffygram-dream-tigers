2e62b44de5230c29ab6488df3f16c0cd25bc9d83
Sean Derevjanik <sean.derevjanik@gmail.com>
2021-11-11 13:11:08 -0700

    corrected missing quotation

--- src/scripts/feed/Post.js
+++ src/scripts/feed/Post.js
@@ -2,11 +2,11 @@ export const Post = (post) => {
     let html = ""
     html += `<section class="post">
                 <header>
-                    <h2 class="post__title>${post.title}</h2>
+                    <h2 class="post__title">${post.title}</h2>
                 </header>
                 <img class="post__image" src=${post.imageURL}>
-                <div class="post__description>${post.description}</div>
-                <div class="post__tagline>placeholder for information for user name of creator and timestamp info</div>
+                <div class="post__description">${post.description}</div>
+                <div class="post__tagline></div>
             </section>`
     return html
 }
